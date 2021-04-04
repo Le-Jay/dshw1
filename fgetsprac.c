ba50c8600f9c811a766881127950fce8b424b314
lejay <jlesl97@naver.com>
2021-04-04 19:43:40 +0900

    ver1

--- fgetsprac.c
+++ fgetsprac.c
@@ -14,11 +14,14 @@ int main()
 
     char* pString = fgets(arr,100,fpString);
     char *pArray = fgets(arr1, 100, fpArray);
-    while(1){
-        if(arr[i]==arr1[i]){
-            num++;
-            index[j] = i;
-            j++;
+    for(i=0; i < ; i++){
+        for(int k=0; k ; k++){
+            if(arr[i]==arr1[k]){
+                index[j]=i; //일치 시작하는 부분의 인덱스(이따 출력용), 일치하는 개수는 이 출력하는 배열의 원소의개수
+                i++;
+            }
+            //pattern의 index k가 끝에 닿으면 다시 0으로
+        }
     }
     printf("num: %d\n",num);
     for(i=0;i<j;i++)

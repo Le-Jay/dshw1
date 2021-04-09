4879ae161196d2cf74baae64bc395275871ef13c
lejay <jlesl97@naver.com>
2021-04-09 13:23:01 +0900

    수정원본

--- fgetsprac.c
+++ fgetsprac.c
@@ -8,36 +8,57 @@
     char arr[MAX_ARR];
     char arr1[MAX_ARR1];
 
+
 int main()
 {
 
     FILE* fpString = fopen("string.txt","r");
     FILE* fpArray = fopen("pattern.txt","r");
+    FILE* fpResult = fopen("result_naive.txt","w");
+
+    if(!fpString){
+        printf("The string file does not exist.\n");
+        return 0;
+    }
+    if(!fpArray){
+        printf("The Pattern file does not exist.\n");
+        return 0;
+    }
 
     char* pString = fgets(arr,100,fpString);
     char *pArray = fgets(arr1, 100, fpArray);
-    strstr(arr,arr1);
-    nfind(arr,arr1);
+
+    int i,j,start = 0;
+
+    int* printarr = (int*)malloc(sizeof(int));
+    *printarr = 0;
+    int index = 0;
+    int lasts = strlen(arr) - 1;
+    int lastp = strlen(arr1) - 1;
+    int endmatch = lastp;
+    for(i=0 ; endmatch <= lasts ; endmatch++, start++){
+        if(arr[endmatch] == arr1[lastp])
+            for(j = 0, i = start ; j < lastp && arr[i] == arr1[j] ; i++, j++);
+            if(j == lastp && (arr[endmatch] == arr1[lastp])){
+                index++;
+                printarr = (int*)realloc(printarr,(index +1) * sizeof(int));
+                ++printarr[0];
+                printarr[index] = start;
+            }
+    }
+    if(fpResult != NULL){
+    fprintf(fpResult,"%d\n",printarr[0]);
+    for(int i = 1; i <= printarr[0]; i++)
+        fprintf(fpResult,"%d ",printarr[i]);
+    }
+    fprintf(fpResult,"\n");
+    printf("Saved on result_naive.txt\n");
+
 
     fclose(pString);
     fclose(pArray);
+    fclose(fpResult);
+    free(printarr);
+
     return 0;
-}
- //for(i=0 ; i<string[i+strlen(pArray)] ; i++)
-int nfind(char *string, char *pat)
-{
-    int num = 0;
-    int i,j,start = 0;
-    int lasts = strlen(string) - 1;
-    int lastp = strlen(pat) - 1;
-    int endmatch = lastp;
-    for(i=0 ; endmatch <= lasts ; endmatch++, start++){
-        if(string[endmatch] == pat[lastp])
-            for(j = 0, i = start ; j < lastp && string[i] == pat[j] ; i++, j++);
-            if(j == lastp && (string[endmatch] == pat[lastp])){
-                num++;
-                printf("%d ",start);
-            }
-    }
-    printf("\n%d",num);
 }
